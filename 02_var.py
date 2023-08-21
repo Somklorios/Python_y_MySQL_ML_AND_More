48916a879988917b0adcfecfdc54726eca6bca77
sercho <sergioa.juarezp@gmail.com>
2023-08-21 01:06:09 -0600

    agregue el archivo 2

---
+++ 02_var.py
@@ -0,0 +1,13 @@
+print("hola, vars")
+
+#estoes una variable con texto
+my_name = "SERGIO"
+print(my_name)
+
+#esto es una variable con un numero
+my_age = 12
+print(my_age)
+
+
+your_name = input("¿Cual es tu nombre?  ")
+print("usando input", your_name)
