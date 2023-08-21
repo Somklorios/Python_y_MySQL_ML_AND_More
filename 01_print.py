aed66271a819b6c929c7b6aec23e2dc50c128403
sercho <sergioa.juarezp@gmail.com>
2023-08-20 23:02:04 -0600

    archivo 01_print.py de python

---
+++ 01_print.py
@@ -0,0 +1,23 @@
+print('Hola mundo que show')
+
+print("extremadamente nuevo ")
+
+
+print(1 + 1)
+print(2 * 2)
+print(3 - 3)
+print(45 / 23)
+
+# esto es un comentario
+
+"""
+esto
+es
+mas 
+sencillo
+"""
+
+'''
+interesante
+mucho
+'''
