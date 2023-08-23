a009909279b5c63107d89ec11b94b28ab4ce3dc7
sercho <sergioa.juarezp@gmail.com>
2023-08-23 15:19:46 -0600

    agregue 3 ejercicios y modifique 1

--- python_simple/05_numbers.py
+++ python_simple/05_numbers.py
@@ -1,14 +1,21 @@
 # importa el modulo de statistics de python , el cual  permite realizar operaciones como suma,promedio,media etc. 
+
 from statistics import mean
+
 # capturar los valores correspondiente a cada mes
+
 budgetEnero = int(input('ingrese el presupuesto de enero '))
 budgetFebrero = int(input('ingrese el presupuesto de febrero '))
 budgetMarzo = int(input('ingrese el presupuesto de marzo '))
+
 # hallar el promedio, dentro de '[]' se puede agregar los valores de los meses separados por comas
+
 mean = mean([budgetEnero, budgetFebrero, budgetMarzo])
 # imprime el promedio
 print(f'el promedio de los meses es {mean}')
 
+
+##################################################################
 lives = 3
 print(type(lives))
 
@@ -23,12 +30,22 @@ print(lives)
 lives = 1
 print(lives)
 
+lives = 12 + 15 
+print(lives)
+
 lives = lives - 1
 print(lives)
 
+
+#forma facil de escribirlo en python
 lives -= 1
 print(lives)
 
+lives -= 5
+print(lives)
+
+lives += 5
+print(lives)
 
 number = 4000000000000000000.1000004
 print(number)
---
+++ python_simple/07_transformation.py
@@ -0,0 +1,23 @@
+name = "Sergio"
+print(type(name))
+name = 12
+print(type(name))
+name = True
+print(type(name))
+
+print("Nicolas" + "Molina")
+print(10 + 12)
+print("Nicolas" + "12")
+
+age = 12
+
+print("mi edad es " + str(age))
+
+#de esta forma podemos darle otro formato ala impresion de python
+print(f"Mi edad es {age}")
+
+age = int(input("Escribe tu edad ---> "))
+print(type(age))
+print(f"Tu edad en 10 años sera {age + 10 }")
+
+
