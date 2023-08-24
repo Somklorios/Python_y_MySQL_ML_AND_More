7e048aeefe60550c16bb8bffeb68f00f3889c468
sercho <sergioa.juarezp@gmail.com>
2023-08-24 01:22:50 -0600

    tres nuevos archivos excelente

---
+++ python_simple/13_if.py
@@ -0,0 +1,29 @@
+if True: 
+  print("deberia ejecutarse")
+
+if False:
+  print("No deberia ejecutarse")
+
+
+pet = input("¿Cual es tu mascota favorita? ")
+
+if pet == "perro":
+  print("genial tienes buen gusto")
+elif pet == 'gato':
+  print('espero tengas suerte')
+elif pet == 'pez':
+  print('eres lo maximo')
+else:
+  print('no tienes ninguna mascota interesante')
+ 
+
+
+'''
+stock = int(input("Digital el stock ==> "))
+
+if stock >= 100 and stock <= 1000:
+  print("el stock es correcto")
+
+else:
+  print("el stock es incorrecto")
+'''
