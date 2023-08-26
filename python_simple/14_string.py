14f39174a68bea099115ca44609601c6e1c3d6bc
sercho <sergioa.juarezp@gmail.com>
2023-08-26 02:24:28 -0600

    agregue dos archivos, proyecto y y 14 string

---
+++ python_simple/14_string.py
@@ -0,0 +1,34 @@
+text = "Ciclopentanoperhidrofenantreno y python es genial aaa"
+
+"""
+print("java" in text)
+print("python" in text)
+
+if "J" in text:
+    print("elegiste bien")
+else:
+    print("elegiste bien")  
+"""
+#Len nos permite saber la longitud de tu string      
+size = len("text")
+print(size)
+
+print(text)
+print(text.upper())
+print(text.lower())
+print(text.count("e"))
+
+print(text.swapcase())
+print(text.startswith("Ella"))
+print(text.endswith("Rust"))
+print(text.endswith("Rust"))
+print(text.replace("python", "go"))
+
+
+text_2 = "este es un titulo"
+print(text_2)
+print(text_2.capitalize())
+print(text_2.title())
+print(text_2.isdigit())
+print("528758273472987293".isdigit())
+
--- python_simple/proyecto.py
+++ python_simple/proyecto.py
@@ -2,8 +2,12 @@ user_option = input('piedra, papel o tijera ----> ')
 user_option == user_option.lower()
 computer_option = 'piedra'
 
+
+#si los dos tiene las mismas opciones es empate
 if user_option == computer_option:
   print('Empate ')
+
+#Si el usuario tiene piedra y la computadora tiene tijera, gana la piedra
 elif user_option == 'piedra':
   if computer_option == 'tijera':
     print('piedra gana a tijera')
@@ -11,7 +15,8 @@ elif user_option == 'piedra':
   else:
     print('piedra gana a tijera')
     print('computer gano')
-    
+
+#Si el usuario tiene papel y la computadora tiene piedra, gana la piedra    
 elif user_option == 'papel':
   if computer_option == 'piedra':
     print('papel gana a piedra')
@@ -19,7 +24,8 @@ elif user_option == 'papel':
   else:
     print('tijera gana a papel')
     print('computer gana')
-    
+
+#Si el usuario tiene tijera y la computadora tiene papel, gana la tijera       
 elif user_option == 'tijera':
   if computer_option == 'papel':
     print('tijera gana a papel')
