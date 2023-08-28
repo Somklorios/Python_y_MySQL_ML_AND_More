0248093d4a517170e018ea5d913f8b697e6db0ea
sercho <sergioa.juarezp@gmail.com>
2023-08-28 02:03:29 -0600

    hice modifcicaciones ya mpliaciones del proyecto papel y tijera

---
+++ python_simple/19_dict.py
@@ -0,0 +1,22 @@
+
+# [ ] = Listas
+# ( ) = Tuplas
+# { } = Diccionarios
+
+my_dict = {}
+
+print(type(my_dict))
+
+my_dict = {
+    'avion': "bla bla bla",
+    'name': "Nicolas",
+    'last_name': "moliuna",
+    'age': 87
+}
+print(my_dict)
+print(len(my_dict))
+
+print(my_dict['age'])
+print(my_dict['last_name'])
+print(my_dict.get['unvalor'])
+
