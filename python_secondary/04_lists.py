550e79c345e42fe71a5ec67b6558eb5633fd12e6
sergio juarez <sergio.juarezp@gmail.com>
2023-09-06 03:29:28 -0600

    Agregue 2 nuevos archivos, uno de ejercicio y el de comprehension

---
+++ python_secondary/04_lists.py
@@ -0,0 +1,21 @@
+# ejecucion sin comprehension
+numbers = []
+for element in range (1, 11):
+    numbers.append(element * 2)
+
+print(numbers)    
+#con comprehension 
+numbers_v2 = [element * 2 for element in range(1, 11)]
+print(numbers_v2)
+
+print("****************************************")
+
+numbers = []
+for i in range(1, 11):
+  if i % 2 == 0:
+     numbers.append(i * 2)
+
+print(numbers)
+#con comprehension 
+numbers_v2 = [i * 2 for i in range(1, 11) if i % 2 == 0]
+print(numbers_v2)
