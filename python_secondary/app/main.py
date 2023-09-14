6c3a03c7c2431acc6974b3c3e7ab4cee6493dd35
sergio juarez <sergio.juarezp@gmail.com>
2023-09-14 16:38:21 -0600

    renombre el mob por utils

---
+++ python_secondary/app/main.py
@@ -0,0 +1,11 @@
+#________Como Construir Módulos____________
+#Para construir módulos debemos tener en cuenta que se deben crear en la misma carpeta a lo cual utilizaremos import para ser llamada en el archivo a trabajar:
+
+#Tenemos un módulo que tiene los datos de la población de 3 paises de Sur America, los cualse vamos a crear en la carpeta app y daremos por nombre utils.py
+
+import utils 
+
+keys, values = mod.get_population()
+print(keys, values)
+
+print(utils.A)
---
+++ python_secondary/app/utils.py
@@ -0,0 +1,19 @@
+def get_population():
+    keys = ["mex", "bol"]
+    values = [300, 400]
+    return keys, values
+
+
+def population_by_country(data, country):
+    result = list(filter(lambda item: item["Country"] == country), data)
+    return result
+
+[{
+	'Country': 'Colombia',
+	'Population': 500
+},
+{
+	'Country': 'Bolivia',
+	'Population': 300
+}
+]
