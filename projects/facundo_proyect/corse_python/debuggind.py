e160932af6826f368f8297602ee1ebc8298cc696
sergio juarez <sergio.juarezp@gmail.com>
2023-09-25 03:57:18 -0600

    modifique debugging

--- projects/facundo_proyect/corse_python/debuggind.py
+++ projects/facundo_proyect/corse_python/debuggind.py
@@ -1,14 +1,16 @@
 def divisors(num):
-    divisors = []
-    for i in range(1, num + 1):
-        if num % i == 0:
-            divisors.append(i)
-    return divisors        
-
+    divisors = [ i for i in range(1, num + 1) if num % i == 0]
+    return divisors
+    
 def run():
-    num = int(input("Ingresa un numero: "))
-    print(divisors(num))
-    print("termino mi programa")
+    try:
+        num = int(input("Ingresa un numero: "))
+        if num < 0:
+            raise ValueError("Solo numeros positivos")
+        print(divisors(num))
+        print("termino mi programa")
+    except ValueError:
+        print("Debes ingresar un numero positivo")    
 
 if __name__ == "__main__":
     run()
