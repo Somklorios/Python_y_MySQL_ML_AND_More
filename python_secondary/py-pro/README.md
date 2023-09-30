4c2e98763d2b4880038a23b08a4779e7463c8ee0
sergio juarez <sergio.juarezp@gmail.com>
2023-09-30 04:27:17 -0600

    add pandas, add new file

--- python_secondary/py-pro/README.md
+++ python_secondary/py-pro/README.md
@@ -6,3 +6,12 @@ python3 -m venv env
 source env/bin/activate
 pip3 install -r requirements.txt
 python3 main.py
+
+
+# Game Project
+
+Para correr el Juego desde seguir esta instrucciones 
+´´´
+cd game
+python3 main.py
+´´´
--- python_secondary/py-pro/app/main.py
+++ python_secondary/py-pro/app/main.py
@@ -6,18 +6,25 @@
 import utils 
 import read_csv
 import charts
-
+import pandas as pd
 
 def run():
-  data = read_csv.read_csv('data.csv')
-
+  '''
   data = list(filter(lambda item: item['Continent'] == 'South America', data))
-
   countries = list(map(lambda x: x['Country/Territory'], data))
   percentages = list(map(lambda x: x['World Population Percentage'], data))
 
+  '''
+
+  df = pd.read_csv('data.csv')
+  df = df[df['Continent'] == 'Africa']
+
+  countries = df['Country/Territory'].values
+  percentages = df['World Population Percentage'].values
+
   charts.generate_pie_chart(countries, percentages)
   
+  data = read_csv.read_csv('data.csv')
   country = input('Type Country => ')
   print(country)
 
@@ -27,7 +34,7 @@ def run():
   if len(result) >  0:
     country = result[0]
     print(country)
-    labels, values = utils.get_population(country)
+    labels, values =  utils.get_population(country)
     charts.generate_bar_chart(country['Country/Territory'], labels, values)
 
 
